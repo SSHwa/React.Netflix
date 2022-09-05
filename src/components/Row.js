73ccaea9310455d5f15e8efcb6fac8381a56a568
showjf <showjf2@gmail.com>
2022-09-05 18:22:09 +0900

    Row 작업

---
+++ src/components/Row.js
@@ -0,0 +1,49 @@
+import axios from "../API/axios";
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+
+function Row({ title, id, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchUrl]);
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    setMovies(request.data.results);
+    console.log("request", request);
+    return request;
+  };
+
+  return (
+    <section className="row">
+      {/* TITLE */}
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow">{"<"}</span>
+        </div>
+        <div id={id} className="row__posters">
+          {/*SEVERAL ROW__POSTER */}
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`${"https://image.tmdb.org/t/p/original/"}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              loading="lazy"
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span className="arrow">{">"}</span>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Row;
